20077d2b5a2a7e96353adee7662b6968009de298
Ivan Castro <73139491+ivancastro87970@users.noreply.github.com>
2021-03-28 11:49:00 +0100

    Update IntegersBT.cpp

--- números inteiros numa árvore binária/IntegersBT.cpp
+++ números inteiros numa árvore binária/IntegersBT.cpp
@@ -39,4 +39,38 @@ vector<int> IntegersBT::inOrder()
 
 size_t IntegersBT::height()
 {
+	/*
+	/*else
+	{
+		size_t vai_left, vai_right;
+		vai_left = height(root.left);
+		vai_right = height(root.right);
+		if (vai_left > vai_right)
+			return vai_left + 1;
+		else
+			return vai_right + 1;
+	}*/
+	/*vector<IntegersBT*> queue1;
+
+	queue1.push_back(root);
+	size_t height = 0;
+
+	while (1)
+	{
+		int node_count = queue1.size();
+		if (node_count == 0)
+			return height;
+
+		height++;
+		while (node_count > 0)
+		{
+			IntegersBT* node = queue1.front();
+			queue1.pop_back();
+			if (node->left != NULL)
+				queue1.push_back(node->left);
+			if (node->right != NULL)
+				queue1.push_back(node->right);
+			node_count --;
+		}
+	}*/
 }
